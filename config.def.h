99bc1869a1ffe94967eaffe12edaa05d415f7c0b
mheaton92 <matthew.heaton92@protonmail.com>
2019-05-29 08:59:47 -0400

    fixed config

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 31;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraMono-Medium:size=12" };
-static const char dmenufont[]       = "FiraMono-Medium=12";
+static const char *fonts[]          = { "FiraCode-Medium:size=12" };
+static const char dmenufont[]       = "FiraCode-Medium=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -27,7 +27,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "t" };
+static const char *tags[] = { " ", " ", "3", "4", "5", "6", "7", "8", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,7 +42,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #include "layouts.c"
 #include "fibonacci.c"
@@ -50,7 +50,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "🧱",      tile },    /* first entry is default */
 	{ "🔎",      monocle },
-	{ "🌐",      grid },
+	{ " ",      grid },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
  	{ "[@]",      spiral },
@@ -72,25 +72,27 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *filecmd[]  = { "st", "ranger", NULL };
-static const char *gotop[]  = { "st", "gotop", NULL };
-static const char *calc[]  = { "st", "bc", NULL };
+/*static const char *filecmd[]  = { "st", "ranger", NULL };*/
+/*static const char *gotop[]  = { "st", "gotop", NULL };*/
+/*static const char *calc[]  = { "st", "bc", NULL };*/
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,			XK_r, 	   spawn,          {.v = filecmd } },
-	{ MODKEY,			XK_i, 	   spawn,          {.v = gotop } },
-	{ MODKEY,			XK_c,	   spawn,          {.v = calc } },
-
+/*	{ MODKEY,			XK_r, 	   spawn,          {.v = filecmd } },*/
+/*	{ MODKEY,			XK_i, 	   spawn,          {.v = gotop } },*/
+/*	{ MODKEY,			XK_c,	   spawn,          {.v = calc } },*/
+	{ MODKEY|ShiftMask,             XK_z,      togglegaps,     {.i = gappx } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,      incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      incnmaster,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_j,      focusmon,       {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_k,      focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -110,7 +112,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_a,      setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_s,      setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_s,      togglesticky,   {0} },
+	{ MODKEY|ShiftMask,              XK_s,      togglesticky,   {0} },
 	/* { MODKEY|ControlMask,	   XK_comma,  cyclelayout,    {.i = -1 } }, */
 	/* { MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } }, */
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
