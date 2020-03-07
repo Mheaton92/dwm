555b1cb6a6791a3432064da8de06417d852848b1
mheaton92 <matthew.heaton92@protonmail.com>
2020-03-07 04:25:10 -0500

    3-7-20

--- config.h
+++ config.h
@@ -36,7 +36,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,     	    0,           -1 },
+	{ "fontpreview",  NULL,       NULL,       0,     	    1,           -1 },
+
 };
 
 /* layout(s) */
